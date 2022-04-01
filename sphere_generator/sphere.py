2d2d7fd700b817b0087b7aff0027198c0ea7a596
Christos Karampatzakis <chriskarampatzakis@gmail.com>
2022-04-01 20:00:59 +0300

    Made generic_sphere more generic.

--- sphere_generator/sphere.py
+++ sphere_generator/sphere.py
@@ -2,49 +2,52 @@ import math
 
 class generic_sphere:
     """
-    This class describes the modeled sphere, either it is in 2 or 3 dimensions,
+    This class describes a generic sphere embedded in a n-dimensional space
     """
 
-    def __init__(self, x =0, y = 0, z = 0, r = 0):
-        """Initialization of the sphere and the dimenstions that it exists
+    def __init__(self, ndim = 3, center = [0,0,0], r = 0):
+        """Initialization of the sphere and the dimensions that it exists
 
         Args:
             x (float): x coordinate of the center
             y (float): y coordinate of the center
             z (float): z coordinate of the center
             r (float): radius of the sphere
-            dimension (int): The dimensions that the sphere exists (2D or 3D space)
         """
 
-        self.x = x
-        self.y = y
-        self.z = z
-        self.r = r
+        self.ndim   = ndim
+        self.center = center
+        self.r      = r
+    
+    #TODO eventually delete, only for compatibility
+    @property
+    def x(self):
+        return self.center[0]
+
+    @property
+    def y(self):
+        return self.center[1]
+
+    @property
+    def z(self):
+        return self.center[2]
     
     def volume(self):
-        pass
-
-class sphere_2D(generic_sphere):
-
-    def __init__(self,xx,yy,rr):
-        super().__init__(xx,yy,0,rr)
-    
-    def volume(self):
-        """calculates the volume of a 2D sphere (disc area) for the current radius
-
-        Returns:
-            Disc Area (float): the disc area
         """
-        return math.pi*(self.r**2)
-    
-class sphere_3D(generic_sphere):
-    def __init__(self,xx,yy,zz,rr):
-        super().__init__(xx,yy,zz,rr)
-           
-    def volume(self):
-        """calculates the volume of a 3D sphere for the current radius
-
-        Returns:
-            Disc Area (float): the disc area
+        Returns the length/area/volume of the 1D/2D/3D sphere.
         """
-        return (4/3)*math.pi*(self.r**3)
+        if self.ndim == 1: 
+            return 2*self.r
+        elif self.ndim == 2:
+            return math.pi*(self.r**2)
+        elif self.ndim == 3:
+            return (4/3)*math.pi*(self.r**3)
+            
+
+class sphere_2D(generic_sphere): #TODO refactor all code referencing it and delete
+    def __init__(self,x,y,radius):
+        super().__init__(ndim = 2, center = (x,y), r = radius)
+    
+class sphere_3D(generic_sphere): #TODO refactor all code referencing it and delete
+    def __init__(self,x,y,z,radius):
+        super().__init__(ndim = 3, center = (x,y,z), r = radius)
