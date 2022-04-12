cf5b6d52ca80010be8dbee2ff654a9159a9ce7ae
clgakias <87116396+clgakias@users.noreply.github.com>
2022-04-12 17:18:36 +0300

    Improved box and sphere modules. Definition of objects refractored.

--- sphere_generator/box.py
+++ sphere_generator/box.py
@@ -2,17 +2,27 @@ class generic_box:
     """
     This class describes the modeled sphere, either it is in 2 or 3 dimensions,
     """
-    
-    sides = (0, 0, 0)
-    def __init__(self, *args):
-        self.sides = args or self.sides
-    
+      
+    def __init__(self, sides = (0, 0, 0)):
+        self.sides =  sides
+
     @property
     def ndim(self):
         return len(self.sides)
 
 class box_2D(generic_box):
-    sides = (0, 0)
+    def __init__(self,sides = (0, 0)):
+        super().__init__(sides)
         
 class box_3D(generic_box):
-    pass
+    def __init__(self, sides = (0, 0, 0)):
+        super().__init__(sides)
+
+#debugging code
+
+box = box_2D((1,1))
+print(box.sides)
+print(box.ndim)
+
+box_2 = box_3D(sides = (1,1,1))
+print(box_2.ndim)
--- sphere_generator/sphere.py
+++ sphere_generator/sphere.py
@@ -2,54 +2,64 @@ import math
 
 class generic_sphere:
     """
-    This class describes the modeled sphere, either it is in 2 or 3 dimensions,
+    This class describes a generic sphere embedded in a n-dimensional space
     """
-    
-    x = 0
-    y = 0
-    z = 0
-    r = 0
 
-    def __init__(self,xx,yy,zz,rr):
-        """Initialization of the sphere and the dimenstions that it exists
-
+    def __init__(self, center = (0,0,0), r = 0):
+        """Initialization of the sphere and the dimensions that it exists
         Args:
-            xx (float): x coordinate of the center
-            yy (float): y coordinate of the center
-            zz (float): z coordinate of the center
-            rr (float): radius of the sphere
-            dimension (int): The dimensions that the sphere exists (2D or 3D space)
+            center (array-like): X,Y,Z coordinates of the center respectively
+            r (float): radius of the sphere
         """
+        self.center = center
+        self.r      = r
+    
+    #TODO eventually delete, only for compatibility
+    @property
+    def x(self):
+        return self.center[0]
 
-        self.x = xx
-        self.y = yy
-        self.z = zz
-        self.r = rr
+    @property
+    def y(self):
+        return self.center[1]
+
+    @property
+    def z(self):
+        return self.center[2]
+    
+    @property
+    def ndim(self):
+        return len(self.center)
     
     def volume(self):
-        pass
+        """
+        Returns the length/area/volume of the 1D/2D/3D sphere.
+        """
+        if self.ndim == 1: 
+            return 2*self.r
+        elif self.ndim == 2:
+            return math.pi*(self.r**2)
+        elif self.ndim == 3:
+            return (4/3)*math.pi*(self.r**3)
+        else:
+            raise ValueError('Volume can only be calculated for 2- or 3- dimensional spheres')
+            
 
 class sphere_2D(generic_sphere):
-
-    def __init__(self,xx,yy,rr):
-        super().__init__(xx,yy,0,rr)
-    
-    def volume(self):
-        """calculates the volume of a 2D sphere (disc area) for the current radius
-
-        Returns:
-            Disc Area (float): the disc area
-        """
-        return math.pi*(self.r**2)
+    def __init__(self,center = (0,0), r = 0):
+        super().__init__(center, r)
     
 class sphere_3D(generic_sphere):
-    def __init__(self,xx,yy,zz,rr):
-        super().__init__(xx,yy,zz,rr)
-           
-    def volume(self):
-        """calculates the volume of a 2D sphere (disc area) for the current radius
+    def __init__(self,center = (0,0,0), r = 0):
+        super().__init__(center, r)
 
-        Returns:
-            Disc Area (float): the disc area
-        """
-        return (4/3)*math.pi*(self.r**3)
+#debugging code
+
+sphere = sphere_2D((1,2),3 )
+print(sphere.ndim)
+print(sphere.r)
+print(sphere.x)
+print(sphere.volume())
+
+sphere_2 = sphere_3D()
+print(sphere_2.volume())
