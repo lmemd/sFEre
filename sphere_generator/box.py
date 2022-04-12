5975e4d53af79d4c3f28fc18fdb96e28132bded5
clgakias <87116396+clgakias@users.noreply.github.com>
2022-04-12 17:48:23 +0300

    Added offsets attribute to box.

--- sphere_generator/box.py
+++ sphere_generator/box.py
@@ -3,26 +3,28 @@ class generic_box:
     This class describes the modeled sphere, either it is in 2 or 3 dimensions,
     """
       
-    def __init__(self, sides = (0, 0, 0)):
+    def __init__(self, sides = (0, 0, 0), offsets = (0, 0, 0)):
         self.sides =  sides
+        self.offsets = offsets
 
     @property
     def ndim(self):
         return len(self.sides)
 
 class box_2D(generic_box):
-    def __init__(self,sides = (0, 0)):
-        super().__init__(sides)
+    def __init__(self,sides = (0, 0), offsets = (0,0)):
+        super().__init__(sides,offsets)
         
 class box_3D(generic_box):
-    def __init__(self, sides = (0, 0, 0)):
-        super().__init__(sides)
+    def __init__(self, sides = (0, 0, 0), offsets = (0, 0, 0)):
+        super().__init__(sides,offsets)
 
 #debugging code
 
 box = box_2D((1,1))
 print(box.sides)
 print(box.ndim)
+print(box.offsets)
 
 box_2 = box_3D(sides = (1,1,1))
 print(box_2.ndim)
