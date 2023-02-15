b8390a154ac801052adbabc60e8c4dac9b8e6cce
Christos Gakias <clgakias@auth.gr>
2023-02-15 19:49:23 +0200

    Revert "A test to optimize box class. Sphere will be next"
    This reverts commit 0877c84494a70572f3cbabb32884b3a4a3fb2347.

--- sphere_generator/box.py
+++ sphere_generator/box.py
@@ -3,16 +3,23 @@ class generic_box:
     This class describes the modeled sphere, either it is in 2 or 3 dimensions,
     """
     
-    sides = (0, 0, 0)
-    def __init__(self, *args):
-        self.sides = args or self.sides
+    dim_x = 0
+    dim_y = 0
+    dim_z = 0
     
-    @property
-    def ndim(self):
-        return len(self.sides)
 
+    def __init__(self,dim_xx,dim_yy,dim_zz):
+        
+        self.dim_x = dim_xx
+        self.dim_y = dim_yy
+        self.dim_z = dim_zz
+        
 class box_2D(generic_box):
-    sides = (0, 0)
+
+    def __init__(self,dim_xx,dim_yy):
+        super().__init__(dim_xx,dim_yy,0)
         
 class box_3D(generic_box):
-    pass
+    def __init__(self,dim_xx,dim_yy,dim_zz):
+        super().__init__(dim_xx,dim_yy,dim_zz)
+
