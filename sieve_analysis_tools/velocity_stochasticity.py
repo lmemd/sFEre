f7040d83f14edf35f10bc1614c9d89fb88ee92e0
clgakias <clgakias@auth.gr>
2023-03-09 17:36:46 +0200

    continue working on velocity stochasticity

--- sieve_analysis_tools/velocity_stochasticity.py
+++ sieve_analysis_tools/velocity_stochasticity.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import truncnorm
-import sieve_analysis_tools.distributions as dist
+import distributions as dist
 
 def normally_distributed_velocity(nominal_velocity, standard_deviation, lower_cuttof = 0, upper_cutoff = np.inf):
     """
@@ -39,10 +39,62 @@ def normally_distributed_velocity(nominal_velocity, standard_deviation, lower_cu
 
 def mixed_velocities(nominal_velocity, percentage_of_retainment, 
                           mean_reduced, std_reduced, reduced_value_range, retained_proportional_factor ):
-    pass
+    
+    lower_bound_percentage_of_retainement = (100 - percentage_of_retainment)/100
+    upper_bound_percentage_of_retainement = 1
+
+    mean_reduced_normalized = mean_reduced/nominal_velocity
+    std_reduced_normalized = std_reduced/nominal_velocity
+
+    lower_value_cutoff = reduced_value_range[0]
+    upper_value_cutoff = reduced_value_range[1]
+
+    return mean_reduced_normalized, std_reduced_normalized, lower_value_cutoff, upper_value_cutoff, lower_bound_percentage_of_retainement, upper_bound_percentage_of_retainement, retained_proportional_factor
 
 def mixed_random_velocities(mean, std, lower_cutoff, upper_cutoff, lower_range, upper_range, gaussian_weight = 0.5):
-    pass
+    """
+    Generates a random number from a mixture of a truncated Gaussian distribution and
+    a uniform distribution with the given mean, standard deviation, range, and cutoff limits.
+    The `gaussian_weight` parameter determines the weight of the Gaussian distribution
+    in the mixture.
+    """
+    # Generate a random number from the truncated Gaussian distribution
+    a = (lower_cutoff - mean) / std
+    b = (upper_cutoff - mean) / std
+    gaussian_num = truncnorm.rvs(a, b, loc=mean, scale=std)
+    # Generate a random number from the uniform distribution
+    uniform_num = np.random.uniform(low=lower_range, high=upper_range)
+    # Calculate the weighted average of the two random numbers
+    arr = np.array([gaussian_num, uniform_num])
+    weights = np.array([gaussian_weight, 1-gaussian_weight])
+    random_num = np.random.choice(arr, p=weights)
+    #mixed_num = gaussian_weight*gaussian_num + (1-gaussian_weight)*uniform_num
+    return random_num
+
+
+test = mixed_velocities(70, 10, 70*0.6, 70*0.27, [0.2,1], 0.75)
+
+
+number = 20000
+vels = []
+for i in range(number):
+    vels.append(mixed_random_velocities(*test))
+
+# Create the histogram
+hist, bins = np.histogram(vels, density=False)
+
+# Calculate the bin width
+bin_width = bins[1] - bins[0]
+
+# Convert the counts to percentages
+hist_percent = (hist / float(len(vels))) * 100.0 * bin_width
+
+# Plot the histogram in percentage
+plt.bar(bins[:-1], hist_percent, width=bin_width)
+plt.xlabel('Data')
+plt.ylabel('Percentage')
+plt.show()
+
 
 
 
