4172f4c2a13d3d851398e039a9416654ac02c1d4
clgakias <clgakias@auth.gr>
2023-03-10 14:00:55 +0200

    completed the implementation of stochastic approach to initial velocities

--- batch_of_spheres_3D_temp.py
+++ batch_of_spheres_3D_temp.py
@@ -8,16 +8,27 @@ from sieve_analysis_tools import sieve_analysis_evaluation as s
 def main():
     #**************************************INPUT SECTION******************************************
     filename_to_export = "demo_batch_of_spheres" # name of sphere file
-    mean_radius = 0.5 # average radius of created sphere
-    radius_std = 0.2 # standard deviation of radius for the created sphere
     spheres_number = 1 # total number of sphere created
-    spheres_batches = 1 # change this variable if you want to create more than one batch of shots
+    spheres_batches = 100 # change this variable if you want to create more than one batch of shots
     shots_material_density = 0.00785 #in gm/mm^3
     nominal_velocity = 70 #in m/s
 
+    #Define the impact velocity configuration
+    retained_initial_velocity = 10 #the velocity range of the shots that retain their initial velocity
+    velocity_reduction_factor = 0.65 #velocity reduction due to shot-shot collision
+    reduced_velocity_standard_deviation_factor = 0.2 #the deviation of reduced velocity
+    velocity_range = [nominal_velocity*0.3 , nominal_velocity*1] #the normalized range for the impact velocities
+    percentage_of_retainment = 50 #the percentage of shots that their initial velocity is reduced
+    velocity_stochasticity_approach = "Mixed Random"
+
+    velocity_params = (nominal_velocity, retained_initial_velocity, 
+                       velocity_reduction_factor*nominal_velocity,
+                       reduced_velocity_standard_deviation_factor*nominal_velocity,
+                       velocity_range, percentage_of_retainment)
+    
+    # Input for sphere generation based on measured sieve analysis data
     sieve_levels = [2., 1.6, 1.4, 1.25, 1.12, 1., 0.9, 0.8, 0.71, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0.]
     retained_weight = [0.0, 0.1, 2.4, 46.3, 42.4, 24.8, 5.6, 5.3, 5.3, 8.8, 9.7, 7.1, 1.7, 0.2, 0.1, 0.0]
-
     sieve_analysis_data = [sieve_levels , retained_weight]
 
     # Define FE length for spheres
@@ -29,6 +40,7 @@ def main():
     box_height = 4 # height of the domain containing the spheres (alongside Y axis)
     box_angle = 90 # change this value if you want an inlcined box (defined by the angle between the box and the XZ plane)
 
+    #define the directory
     parent_dir = os.getcwd()
     directory = parent_dir + '/generated_spheres/' #the directory for the final output
 
@@ -43,6 +55,7 @@ def main():
     #***********************************END OF INPUT SECTION**************************************
 
     spheres_list = [] # initialize empty spheres list
+    velocities_list = [] # initialize empty applied velocities list
     for set_number in range(spheres_batches):
 
         # Generate stream of random distributed shots in space
@@ -64,13 +77,10 @@ def main():
 
         # Call this function if you want to apply initial velocity to the shot stream, in LSDYNA keyword format.
         # Currently, an absolute initial velocity of 70 m/s will be applied.
-        stream_velocity_configuration = (nominal_velocity,nominal_velocity/5)
-        
-        
-        
-        
-        apply_initial_velocity(filename, nominal_velocity, "Normal distribution", *stream_velocity_configuration, angle = box_angle)
 
+        applied_velocity = apply_initial_velocity(filename, nominal_velocity, velocity_stochasticity_approach, *velocity_params, angle = box_angle)
+
+        velocities_list.extend(applied_velocity)
         spheres_list.extend(spheres)
         print(set_number)
     
@@ -81,7 +91,6 @@ def main():
     generated_diameters = [s.r*2 for s in spheres_list]
     
     s.evaluate(sieve_levels, retained_weight, generated_diameters, shots_material_density)
-
 
     # Print the X,Y coordinates and the radii of created spheres
     #list_to_print = ['%.4f'%s.x + '    ' +  '%.4f'%s.y + '   ' +  '%.4f'%s.z + '    ' +  '%.4f'%s.r for s in spheres]
