12fdea359f79455853a202523a53a013cf99c3b6
Kourampas <kourampap@meng.auth.gr>
2025-10-09 22:23:48 +0300

    Update sphere_mesh.py

--- FE_mesh/sphere_mesh.py
+++ FE_mesh/sphere_mesh.py
@@ -4,7 +4,7 @@
 3. Mesh quality criteria should be created
 '''
 import numpy as np
-import numpy_indexed as npi
+#import numpy_indexed as npi
 
 from numpy.lib.twodim_base import mask_indices
 
@@ -365,7 +365,8 @@ def renumbering_element_pairs(old_nodes_id, sorted_nodes_id, elements_matrix):
         ndarray : Mapped elements matrix.
     """
     mapping = dict(zip(old_nodes_id, sorted_nodes_id))
-    elements_matrix = npi.remap(elements_matrix.flatten(), list(mapping.keys()), list(mapping.values())).reshape(np.shape(elements_matrix)[0], np.shape(elements_matrix)[1])
+    #CHANGE FROM npi TO np
+    elements_matrix = np.remap(elements_matrix.flatten(), list(mapping.keys()), list(mapping.values())).reshape(np.shape(elements_matrix)[0], np.shape(elements_matrix)[1])
 
     return elements_matrix
 
